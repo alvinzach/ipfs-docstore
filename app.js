621edcc5c0a392c08e1f5584b3eccb85c7b53b9d
alvinzach <alvinzach@gmail.com>
2018-10-06 20:03:51 +0530

    Using multer to upload files

--- app.js
+++ app.js
@@ -1,36 +1,49 @@
 
-const express = require('express');
-const fs = require('fs');
-var bodyParser = require('body-parser')
-var app = express()
+const express = require('express')
+const fs = require('fs')
+const multer=require('multer')
+const bodyParser = require('body-parser')
+const app = express()
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now()+file.originalname)
+    }
+  })
+   
+var upload = multer({ storage: storage })
+const IPFS = require('ipfs-mini')
+const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' })
 
-const IPFS = require('ipfs-mini');
-const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
 
-
-app.post('/addfile', function(req, res) {
-    let testFile = fs.readFileSync(req.body.path);
-    let testBuffer = new Buffer(testFile);
-    ipfs.addJSON({path:req.body.path,content:testBuffer}, function (err, result) {
+app.post('/addfile',upload.single('file'),function(req, res) {
+    console.log(req.file.path)
+    var doc=fs.readFileSync(req.file.path)
+    var docBuffer=new Buffer(doc)
+    ipfs.addJSON({path:req.file.path,type:req.file.mimetype,content:docBuffer}, function (err, result) {
         if (err) {
             res.json({status:400,error:err})
         }else{
-            fs.unlinkSync(req.body.path)
+            fs.unlinkSync(req.file.path)
             res.json({status:200,hash:result})
         }
       })
 
 })
 app.post('/getfile', function(req, res) {
-    
-    ipfs.catJSON(req.body.hash, function (err, files) {
+    console.log(req.body.hash)
+    ipfs.catJSON(req.body.hash,function (err, doc) {
         if (err) {
-            res.json({status:400,error:err})
+            console.log(err)
+            res.json({status:400,error:err.toString()})
         }else{
-            fs.writeFileSync(files.path,Buffer(files.content.data))
-            res.json({status:200,path:files.path})
+            fs.writeFileSync(doc.path,Buffer(doc.content.data))
+            res.download(doc.path)
         }
       })
 
